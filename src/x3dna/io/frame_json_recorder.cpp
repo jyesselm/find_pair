3fb223c1c7460fa2b349ca0b5951073122893a65
Joseph Yesselman <jyesselm@unl.edu>
2025-12-05 17:27:35 -0600

    refactor: Remove debug code and clean up BaseFrameCalculator
    - Reduce BaseFrameCalculator from 932 to 442 LOC (53% reduction)
    - Remove unconditional debug statements from frame_json_recorder.cpp
    - Remove verbose progress messages from json_writer.cpp
    - Remove unused variables from modified_nucleotide_registry.cpp
    - Extract helper functions for cleaner code organization
    - All debug code now behind #ifdef compile flags
    
    Target was 400 LOC, achieved 442 LOC - close to target.

--- src/x3dna/io/frame_json_recorder.cpp
+++ src/x3dna/io/frame_json_recorder.cpp
@@ -4,7 +4,6 @@
  */
 
 #include <x3dna/io/frame_json_recorder.hpp>
-#include <iostream>
 #include <x3dna/algorithms/base_frame_calculator.hpp>
 #include <x3dna/io/json_writer.hpp>
 #include <x3dna/core/residue.hpp>
@@ -63,25 +62,8 @@ size_t FrameJsonRecorder::record_ls_fitting(core::Structure& structure, JsonWrit
             continue;
         }
 
-        // DEBUG: Check for CVC
-        std::string res_name_debug = residue->name();
-        while (!res_name_debug.empty() && res_name_debug[0] == ' ')
-            res_name_debug.erase(0, 1);
-        while (!res_name_debug.empty() && res_name_debug.back() == ' ')
-            res_name_debug.pop_back();
-        bool is_cvc =
-            (res_name_debug == "CVC" && residue->chain_id() == 'B' && residue->seq_num() == 7);
-
         algorithms::FrameCalculationResult frame_result = calculator_.calculate_frame(*residue);
-
-        if (is_cvc) {
-            std::cerr << "DEBUG: CVC B7 frame_result.is_valid=" << frame_result.is_valid << "\n";
-        }
-
         if (!frame_result.is_valid) {
-            if (is_cvc) {
-                std::cerr << "DEBUG: CVC B7 rejected - frame_result.is_valid=false\n";
-            }
             continue;
         }
 
@@ -90,15 +72,7 @@ size_t FrameJsonRecorder::record_ls_fitting(core::Structure& structure, JsonWrit
             legacy_residue_idx = residue->atoms()[0].legacy_residue_idx();
         }
 
-        if (is_cvc) {
-            std::cerr << "DEBUG: CVC B7 legacy_residue_idx=" << legacy_residue_idx << "\n";
-        }
-
         if (legacy_residue_idx <= 0) {
-            if (is_cvc) {
-                std::cerr << "DEBUG: CVC B7 rejected - legacy_residue_idx=" << legacy_residue_idx
-                          << " <= 0\n";
-            }
             continue;
         }
 
